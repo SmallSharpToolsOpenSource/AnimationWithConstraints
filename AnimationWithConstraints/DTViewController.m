f2de348c80a3f643cd10b2a106a0c50927be8ed3
Brennan Stehling <brennan@smallsharptools.com>
2012-10-22 14:18:24 -0500

    updated to use buttons instead of segment control

--- AnimationWithConstraints/DTViewController.m
+++ AnimationWithConstraints/DTViewController.m
@@ -12,18 +12,21 @@
 
 @interface DTViewController ()
 
-@property (weak, nonatomic) IBOutlet UIView *box1;
-@property (weak, nonatomic) IBOutlet UIView *box2;
-@property (weak, nonatomic) IBOutlet UIView *box3;
-
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *box1TopConstraint;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *box2TopConstraint;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *box3TopConstraint;
 
-@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
-@property (weak, nonatomic) IBOutlet UIButton *pushButton;
+@property (weak, nonatomic) IBOutlet UIView *box1;
+@property (weak, nonatomic) IBOutlet UIView *box2;
+@property (weak, nonatomic) IBOutlet UIView *box3;
 
-- (IBAction)segmentValueChanged:(id)sender;
+@property (weak, nonatomic) IBOutlet UILabel *instructionsLabel;
+
+@property (weak, nonatomic) IBOutlet UIButton *button1;
+@property (weak, nonatomic) IBOutlet UIButton *button2;
+@property (weak, nonatomic) IBOutlet UIButton *button3;
+
+@property (weak, nonatomic) IBOutlet UIButton *pushButton;
 
 @end
 
@@ -31,6 +34,7 @@
     CGFloat box1OriginY;
     CGFloat box2OriginY;
     CGFloat box3OriginY;
+    CGFloat movementDistance;
 }
 
 - (void)viewDidLoad {
@@ -40,6 +44,17 @@
     [self setBackgroundImage:[UIImage imageNamed:@"GreenButton-Dark"] forButton:self.pushButton forState:UIControlStateNormal];
     [self setBackgroundImage:[UIImage imageNamed:@"GreenButton-Light"] forButton:self.pushButton forState:UIControlStateHighlighted];
     [self setBackgroundImage:[UIImage imageNamed:@"GreenButton-Light"] forButton:self.pushButton forState:UIControlStateSelected];
+    
+    [self decorateButton:self.pushButton];
+    [self decorateButton:self.button1];
+    [self decorateButton:self.button2];
+    [self decorateButton:self.button3];
+}
+
+- (void)decorateButton:(UIButton *)button {
+    [self setBackgroundImage:[UIImage imageNamed:@"GreenButton-Dark"] forButton:button forState:UIControlStateNormal];
+    [self setBackgroundImage:[UIImage imageNamed:@"GreenButton-Light"] forButton:button forState:UIControlStateHighlighted];
+    [self setBackgroundImage:[UIImage imageNamed:@"GreenButton-Light"] forButton:button forState:UIControlStateSelected];
 }
 
 - (void)setBackgroundImage:(UIImage *)image forButton:(UIButton *)button forState:(UIControlState)inState {
@@ -56,52 +71,52 @@
 }
 
 - (void)viewDidLayoutSubviews {
-    box1OriginY = self.box1.frame.origin.y;
-    box2OriginY = self.box2.frame.origin.y;
-    box3OriginY = self.box3.frame.origin.y;
+    if (box1OriginY == 0.0) {
+        box1OriginY = self.box1.frame.origin.y;
+        box2OriginY = self.box2.frame.origin.y;
+        box3OriginY = self.box3.frame.origin.y;
+        
+        movementDistance = self.instructionsLabel.frame.origin.y - self.box1.frame.size.height;
+    }
 }
 
-- (IBAction)segmentValueChanged:(id)sender {
-    if ([self.segmentedControl isEqual:sender]) {
-        DebugLog(@"Value Changed: %i", self.segmentedControl.selectedSegmentIndex);
-        
-        if (self.segmentedControl.selectedSegmentIndex == 0) {
-            if (kUseConstraintsAnimation) {
-                [self toggleBoxAnimation:self.box1 topConstraint:self.box1TopConstraint];
-            }
-            else {
-                [self toggleBoxAnimation:self.box1 originY:box1OriginY];
-            }
-        }
-        else if (self.segmentedControl.selectedSegmentIndex == 1) {
-            if (kUseConstraintsAnimation) {
-                [self toggleBoxAnimation:self.box2 topConstraint:self.box2TopConstraint];
-            }
-            else {
-                [self toggleBoxAnimation:self.box2 originY:box2OriginY];
-            }
-        }
-        else if (self.segmentedControl.selectedSegmentIndex == 2) {
-            if (kUseConstraintsAnimation) {
-                [self toggleBoxAnimation:self.box3 topConstraint:self.box3TopConstraint];
-            }
-            else {
-                [self toggleBoxAnimation:self.box3 originY:box3OriginY];
-            }
-        }
+- (IBAction)button1Tapped:(id)sender {
+    if (kUseConstraintsAnimation) {
+        [self toggleBoxAnimation:self.box1 topConstraint:self.box1TopConstraint];
+    }
+    else {
+        [self toggleBoxAnimation:self.box1 originY:box1OriginY];
+    }
+}
+
+- (IBAction)button2Tapped:(id)sender {
+    if (kUseConstraintsAnimation) {
+        [self toggleBoxAnimation:self.box2 topConstraint:self.box2TopConstraint];
+    }
+    else {
+        [self toggleBoxAnimation:self.box2 originY:box2OriginY];
+    }
+}
+
+- (IBAction)button3Tapped:(id)sender {
+    if (kUseConstraintsAnimation) {
+        [self toggleBoxAnimation:self.box3 topConstraint:self.box3TopConstraint];
+    }
+    else {
+        [self toggleBoxAnimation:self.box3 originY:box3OriginY];
     }
 }
 
 - (void)toggleBoxAnimation:(UIView *)box originY:(CGFloat)originY {
     CGRect frame = box.frame;
     if (originY == frame.origin.y) {
-        frame.origin.y += 100.0;
+        frame.origin.y += movementDistance - 30.0;
     }
     else {
         frame.origin.y = originY;
     }
     
-    UIViewAnimationOptions options = UIViewAnimationOptionAllowUserInteraction;
+    UIViewAnimationOptions options = UIViewAnimationOptionAllowUserInteraction | UIViewAnimationOptionBeginFromCurrentState;
     [UIView animateWithDuration:0.5 delay:0.0 options:options animations:^{
         box.frame = frame;
     } completion:^(BOOL finished) {
@@ -109,11 +124,10 @@
 }
 
 - (void)toggleBoxAnimation:(UIView *)box topConstraint:(NSLayoutConstraint *)topConstraint {
-    
-    UIViewAnimationOptions options = UIViewAnimationOptionAllowUserInteraction;
+    UIViewAnimationOptions options = UIViewAnimationOptionAllowUserInteraction | UIViewAnimationOptionBeginFromCurrentState;
     [UIView animateWithDuration:0.5 delay:0.0 options:options animations:^{
         if (topConstraint.constant == 20.0) {
-            topConstraint.constant = 100.0;
+            topConstraint.constant = movementDistance - 10.0;
         }
         else {
             topConstraint.constant = 20.0;
